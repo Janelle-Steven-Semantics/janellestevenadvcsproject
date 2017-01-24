bf721b250a28ee5b8fba9d63c90ab8724e93fecb
stevenburke <stevenburke@stmarksschool.org>
2017-01-24 10:42:06 -0500

    Attempt to Count words

--- Book/__init__.py
+++ Book/__init__.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
+from PyDictionary import PyDictionary
+dictionary = PyDictionary()
 
+print(dictionary.get_synonym("happy"))
 sample = open("Loadbook.txt")
 for line in sample:
     # insert any "word" that you want to search for
-    if "great" in line:
+    if dictionary.get_synonym("happy") in line:
         print(line)
         i + 1
         print(i)
