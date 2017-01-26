a9c1fcbb57914e26a28bb220b59294d769596279
stevenburke <stevenburke@stmarksschool.org>
2017-01-26 14:58:44 -0500

    Beginning of Thesaurus

--- Book/__init__.py
+++ Book/__init__.py
@@ -8,8 +8,21 @@ for line in sample:
     # insert any "word" that you want to search for
     if dictionary.get_synonym("happy") in line:
         print(line)
-        i + 1
-        print(i)
+        wordcount + 1
+        print("There are " + wordcount + "Occurrences Like Happy")
 
+
+        #What parameters determine tone?
+
+
+
+    #check every 4 lines
+
+        # count some indiscriminate synonyms OR Synonyms from book
+
+        # do some math good - bad
+            #if 0 bad = good
+            # if bad > good tone = bad
+                # if difference small or big
 
 
