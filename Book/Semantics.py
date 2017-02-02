dadc86eccb37247cc5d841eb211431284649b6fe
Janelle Carmichael <janellecarmichael@stmarksschool.org>
2017-02-02 15:08:35 -0500

    Improved Windows-friendly virtualenv .gitignore
    ...and some other stuff.

---
+++ Book/Semantics.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import PyDictionary
+dictionary = PyDictionary()
+
++print(dictionary.get_synonym("happy"))
+sample = open("Loadbook.txt")
+
+for line in sample:
+    # insert any "word" that you want to search for
+    wordsearched= "great"
+    if wordsearched in line:
+        print(line)
+     print("There are " + wordcount + "Occurrences Like Happy")
+
