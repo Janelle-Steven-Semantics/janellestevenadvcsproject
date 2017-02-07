7ec7dfb46ce049090de6aac8d967782b35f074ec
stevenburke <stevenburke@stmarksschool.org>
2017-02-07 10:39:24 -0500

    Tone searches for multiple words

--- Book/__init__.py
+++ Book/__init__.py
@@ -7,9 +7,9 @@ searchlist = ['happy','sad','good','bad']
 
 
 for listword in range(len(searchlist)):
-    word = listword # the word we're looking for (including synonyms)
-    wordcount = 0 # how many instances of the word
-    wordlist = [] # what synonyms we found
+    word = searchlist[listword]  # the word we're looking for (including synonyms)
+    wordcount = 0  # how many instances of the word
+    wordlist = []  # what synonyms we found
 
 # open up the book!
     sample = open("Loadbook.txt")
@@ -18,19 +18,19 @@ for listword in range(len(searchlist)):
     for line in sample:
     
     # is our search term in the line?
-      if word in line:
+        if word in line:
             if word not in wordlist:
                 wordlist.append(word)
             print(line)
             wordcount = wordcount + line.count(word)
         
     # are any synonyms in the line?
-    for w in dictionary.synonym(word):
-        if w in line:
-            if w not in wordlist:
-                wordlist.append(w)
-            print(line)
-            wordcount = wordcount + line.count(w)
+        for w in dictionary.synonym(word):
+             if w in line:
+                if w not in wordlist:
+                    wordlist.append(w)
+                    print(line)
+                    wordcount = wordcount + line.count(w)
 
 # print out the results
     print("There are {} Occurrences like {} ({})").format(wordcount, word, wordlist)
