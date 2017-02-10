db065c3422c40c10b751ab566721f2cc1ad9a647
stevenburke <stevenburke@stmarksschool.org>
2017-02-10 17:59:50 -0500

    Hey Janelle this counts the line number, but I am unsure how to use it.  Can you please figure out how to use it and check my code? Thanks! Amendment Just updated some errors to it

--- Book/__init__.py
+++ Book/__init__.py
@@ -16,14 +16,14 @@ for listword in range(len(searchlist)):
     linecount = 0
 # walk through the book, line by line
     for line in sample:
-        linecount = linecount + 1
+        linecount = linecount + 1  #counts the line number
     # is our search term in the line?
         if word in line:
             if word not in wordlist:
                 wordlist.append(word)
             print(line)
             wordcount = wordcount + line.count(word)
-            print("Occurs on line " + linecount)
+            print("Occurs on line " + str(linecount))
     # are any synonyms in the line?
         for w in dictionary.synonym(word):
              if w in line:
@@ -31,6 +31,6 @@ for listword in range(len(searchlist)):
                     wordlist.append(w)
                     print(line)
                     wordcount = wordcount + line.count(w)
-                    print("Occurs on line " + linecount)
+                    print("Occurs on line " + str(linecount))
 # print out the results
     print("There are {} Occurrences like {} ({})").format(wordcount, word, wordlist)
