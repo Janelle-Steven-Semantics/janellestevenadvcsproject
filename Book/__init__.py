d2d8a0c128ef7a67e20fd88532f28ecf8fd8dd62
stevenburke <stevenburke@stmarksschool.org>
2017-03-09 12:56:27 -0500

    This is what I have, could you please figure out why my if statement is never true?

--- Book/__init__.py
+++ Book/__init__.py
@@ -4,7 +4,6 @@ from Synonym_extension import Synonym_extension
 from PyDictionary import PyDictionary
 import nltk
 from PyDictionary import PyDictionary
-nltk.download()
 from nltk.tokenize import sent_tokenize
 from nltk.book import*
 from nltk.corpus import*
@@ -22,7 +21,9 @@ terms = [
     'fear',
     'anxious',
     'life',
-    'gloomy'
+    'gloomy',
+    'dark',
+    'light'
 ]
 searchResults = []
 for term in terms:
@@ -36,74 +37,66 @@ tokens = nltk.sent_tokenize(book)
 #told you the tokenizer would come in handy
 #searches the broken down sentences
 #prints in order--we definitely need the line numbers printed
-
+tone = 0
+termsbad = [
+    'sad',
+    'bad',
+    'death',
+    'fear',
+    'anxious',
+    'gloomy',
+    'dark',]
+termsgood = [
+    'happy',
+    'good',
+    'peace',
+    'light']
 for token in tokens:
     for term in searchResults:
         if term.term in token:
             term.increment()
-            print(token)
+            print(token + "/////")
+            #tone.count = term.count
+            tone = str(term)
+            #print(tone.count)
+            if term in termsbad:
+                print("tone is darker")
+                print(term)
+
+                if tone in ['sad']:
+                    print("tone is sad")
+                    print(term)
+            if term in termsgood:
+                print("tone is lighter")
+                print(term)
+                if tone in 'happy':
+                    print("tone is happy")
+                    print(term)
+
+
+
+
+
         for synonym in term.synonyms:
             if synonym in token:
                 term.increment(synonym)
-                print(token)
+                print(token + "/////")
+                #tone.count = term.count
+                #print(tone.count)
+                tone = str(term)
+                if term in termsbad:
+                    print("tone is darker")
+                    print(str(term))
 
-for term in searchResults:
- print('There are {} occurrences of synonyms of {} (using {} as synonyms)'.format(term.count, term.term, term.synonyms))
-book = open('loadbook.txt')
-for line in book:
-    for term in searchResults:
-        if term.term in line:
-            term.increment()
-
-        for synonym in term.synonyms:
-            if synonym in line:
-                term.increment(synonym)
-
+                    if tone in ['sad']:
+                        print("tone is sad")
+                        print(str(term))
+                if term in termsgood:
+                    print("tone is lighter")
+                    print(str(term))
+                    if tone in 'happy':
+                        print("tone is happy")
+                        print(str(term))
 for term in searchResults:
     print('There are {} occurrences of synonyms of {} (using {} as synonyms)'.format(term.count, term.term, term.synonyms))
-
-
-
-
-
-usablewordlist = [searchlist]
-for j in range(len(usablewordlist)):
-    usablewordlist.insert(j, wordcount)
-
-highesttone = max(usablewordlist)
-print(highesttone)
-#n = 0
-#for n in range(len(usablewordlist)):
-    #n = n + 1
-    #if usablewordlist[2n-1] > usablewordlist[2n]:
-        #print("the tone for line " + str(linecount) + " sorta good")
-if usablewordlist[1] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " Happy")
-if usablewordlist[2] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " Sad")
-if usablewordlist[3] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " good")
-if usablewordlist[4] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " bad")
-if usablewordlist[5] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " peace")
-if usablewordlist[6] == max(usablewordlist):
-    if usablewordlist[3] != 0:
-        print("the tone for line " + str(linecount) + " humorous")
-    else:
-        print("the tone for line " + str(linecount) + " death")
-if usablewordlist[7] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " light")
-if usablewordlist[8] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " fear")
-if usablewordlist[9] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " humour")
-if usablewordlist[10] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " anxious")
-if usablewordlist[11] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " life")
-if usablewordlist[12] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " gloomy")
-if usablewordlist[13] == max(usablewordlist):
-    print("the tone for line " + str(linecount) + " respect")
 
