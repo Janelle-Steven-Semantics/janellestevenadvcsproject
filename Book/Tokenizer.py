1eec280c210b1ee14e06fba27a8e4ca068d4fc8d
Janelle Carmichael <janellecarmichael@stmarksschool.org>
2017-04-20 12:03:56 -0400

    Started our webapp, Loadbook
    Boring app name--sorry :/

--- Book/Tokenizer.py
+++ Book/Tokenizer.py
@@ -2,7 +2,7 @@ import nltk
 from book.ntlk import *
 nltk.download()
 from nltk.tokenize import word_tokenize, sent_tokenize
-sample=open("Loadbook.txt").read()
+sample=open("loadbook.txt").read()
 tokens=sent_tokenize(sample)
 tokens=tokens[1:5]
 sample.concordance("Dashwood")
