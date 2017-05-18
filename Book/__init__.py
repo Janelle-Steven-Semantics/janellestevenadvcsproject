f1adecb6f8c7e1b8075a33733efcc0775638ae5b
Janelle Carmichael <janellecarmichael@stmarksschool.org>
2017-05-17 23:17:57 -0400

    Adding the visuals; color codes tokens into HTML file
    Getting better

--- Book/__init__.py
+++ Book/__init__.py
@@ -12,23 +12,8 @@ from PyDictionary import PyDictionary
 thesaurus = PyDictionary()
 import sys
 import fileinput
-
-f = open('test.txt', 'r+')
-
-try:
-    f2 = open('Loadbook.txt','a')
-except:
-    print('something went wrong ')
-    sys.exit(0)
-
-
-for token in f:
-    if 'Dashwood' in token:
-       token= token.replace('Dashwood', 'batman')
-      # print("Alexa" + " " + line)
-       f2.write(token)
+f = open('output.html', 'r+')
 sys.stdout = f
-
 
 
 terms = [
@@ -47,6 +32,7 @@ terms = [
     'light',
     'worried',
     'confident',
+    'charming',
     'cruel',
     'aggressive',
     'harm',
@@ -57,6 +43,8 @@ terms = [
     'afraid',
     'regret',
     'scold',
+    'friendly',
+    'generous'
     ]
 searchResults = []
 for term in terms:
@@ -98,28 +86,31 @@ termsgood = [
     'confident',
     'light',
     'wonderful',
-    'hope'
+    'hope',
+    'charming',
+    'generous'
 ]
 termspeaceful = [
     'peace',
     'calm',
     'easy']
-termscandid = [
-    'offense',
-    'rude',
-    'truth',
-]
+
+
 termsaggressive = [
     'aggressive',
     'harm',
     'anger',
     'scold'
+    'offense',
+    'rude',
+
 ]
 termshappy = [
     'happy',
     'good',
     'laugh',
     'joy',
+    'friendly'
 ]
 
 badprefixes= [
@@ -135,64 +126,86 @@ cacount = 0
 hacount = 0
 tokencount = 0;
 
+def color_positive(f, token):
+    f.write('<p style="color:#63EB19">%s</p>' % token)
+
+def color_negative(f, token):
+    f.write('<p style="color:#FF0000">%s</p>' % token)
+
+def color_ominous(f, token):
+    f.write('<p style="color:#E9EC1A">%s</p>' % token)
+
+
 for token in tokens:
     ++tokencount
     for term in searchResults:
         if term.term in token and term.term in termsgood and not term.term.startswith(tuple(badprefixes)):
-            print("/////This tone is good!")
+            term.increment()
+            color_positive(f, token)
+            ++gocount
+
 
         if term.term in token and term.term in termsbad:
             term.increment()
+            color_negative(f, token)
             ++bacount
-            print("/////This tone is bad!")
+
+
         if term.term in token and term.term in termsominous:
             term.increment()
+            color_ominous(f, token)
             ++omcount
-            print(token)
-            print("/////This tone is ominous!")
+
         if term.term in token and term.term in termspeaceful and not term.term.startswith(tuple(badprefixes)):
             term.increment()
+            color_positive(f, token)
             ++pecount
-            print("/////This tone is peaceful!")
+
         if term.term in token and term.term in termsaggressive:
             term.increment()
+            color_negative(f, token)
             ++agcount
-            print("/////This tone is aggressive!")
-        if term.term in token and term.term in termscandid:
-            term.increment()
-            ++cacount
-            print("/////This tone is candid!")
+
         if term.term in token and term.term in termshappy and not term.term.startswith(tuple(badprefixes)):
             term.increment()
+            color_positive(f, token)
             ++hacount
-            print("/////This tone is Happy!")
+
+        else:
+            print(token)
 
 
         for synonym in term.synonyms:
-            if synonym in token and term.term in termsgood and not term.term.startswith(tuple(badprefixes)):
+            if synonym in token and synonym not in terms and term.term in termsgood and not term.term.startswith(tuple(badprefixes)):
+                #print("The tone of this sentence is also partly good.")
                 term.increment(synonym)
                 ++gocount
-                print("/////This tone is good! :)")
-            if synonym in token and term.term in termsbad:
+
+            if synonym in token and synonym not in terms and term.term in termsbad:
+                #print("The tone of this sentence is also partly bad.")
                 term.increment(synonym)
                 ++bacount
-                print("/////This tone is bad!")
-            if synonym in token and term.term in termsominous:
+
+            if synonym in token and synonym not in terms and term.term in termsaggressive:
+                #print("The tone of this sentence is also partly aggressive.")
+                term.increment(synonym)
+                ++agcount
+
+            if synonym in token and synonym not in terms and term.term in termsominous:
+                #print("The tone of this sentence is also partly ominous.")
                 term.increment(synonym)
                 ++omcount
-                print("/////This tone is ominous! :)")
-            if synonym in token and term.term in termspeaceful and not term.term.startswith(tuple(badprefixes)):
+
+            if synonym in token and synonym not in terms and term.term in termspeaceful and not term.term.startswith(tuple(badprefixes)):
+                #print("The tone of this sentence is also partly aggressive.")
                 term.increment(synonym)
                 ++pecount
-                print("/////This tone is peaceful!")
-            if synonym in token and term.term in termscandid:
-                term.increment(synonym)
-                ++cacount
-                print("/////This tone is candid! :)")
-            if synonym in token and term.term in termshappy and not term.term.startswith(tuple(badprefixes)):
+
+            if synonym in token and synonym not in terms and term.term in termshappy and not term.term.startswith(tuple(badprefixes)):
+                #print("The tone of this sentence is also partly happy.")
                 term.increment(synonym)
                 ++hacount
-                print("/////This tone is Happy!")
+
 
 tokecount = 0
 
@@ -227,4 +240,3 @@ for term in searchResults:
     print('There are {} occurrences of synonyms of {} (using {} as synonyms)'.format(term.count, term.term, term.synonyms))
 
 f.close()
-f2.close()
